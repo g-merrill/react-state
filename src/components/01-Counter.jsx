cb27239270ef5471204a38263c9380609b7c0ecc
Greg Merrill <merrillgreg@meta.com>
2025-06-10 10:18:07 -0700

    refactor

--- src/components/01-Counter.jsx
+++ src/components/01-Counter.jsx
@@ -1,15 +1,24 @@
 import { useState } from "react"
 
 export default function Counter() {
-  const [count, setCount] = useState(0)
-  return (
-    <div>
-      <h2>Counter</h2>
-      <h3>{count}</h3>
-      <div className="button-group">
-        <button onClick={() => setCount(count + 1)}>Increase</button>
-        <button onClick={() => setCount(count - 1)}>Decrease</button>
-      </div>
-    </div>
-  )
+	const [count, setCount] = useState(0)
+
+	const inc = () => {
+		setCount(count + 1)
+	}
+
+	const dec = () => {
+		setCount(count - 1)
+	}
+
+	return (
+		<div>
+			<h2>Counter</h2>
+			<h3>{count}</h3>
+			<div className="button-group">
+				<button onClick={inc}>Increase</button>
+				<button onClick={dec}>Decrease</button>
+			</div>
+		</div>
+	)
 }
