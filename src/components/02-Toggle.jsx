0890957d66adf2ccd916ed38cfbc32838179614e
Greg Merrill <merrillgreg@meta.com>
2025-06-10 10:21:38 -0700

    add toggle logic

--- src/components/02-Toggle.jsx
+++ src/components/02-Toggle.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react"
+
 export default function Toggle() {
+  const [timeToggle, setTimeToggle] = useState(false)
   return (
     <div className="App">
       <h2>Toggle</h2>
-      <div className="emoji">💡 🌌</div>
+      <div className="emoji">{timeToggle ? <span>🌌</span> : <span>💡</span>}</div>
       <div className="button-group">
-        <button>Day</button>
-        <button>Night</button>
-        <button>Toggle</button>
+        <button onClick={() => setTimeToggle(false)}>Day</button>
+        <button onClick={() => setTimeToggle(true)}>Night</button>
+        <button onClick={() => setTimeToggle(!timeToggle)}>Toggle</button>
       </div>
     </div>
   )
