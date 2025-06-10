7daade96337cf2f6163bed5926be952e1fb0eefb
Greg Merrill <merrillgreg@meta.com>
2025-06-10 10:34:00 -0700

    finish toggle logic

--- src/components/02-Toggle.jsx
+++ src/components/02-Toggle.jsx
@@ -1,15 +1,22 @@
 import { useState } from "react"
 
 export default function Toggle() {
-  const [timeToggle, setTimeToggle] = useState(false)
+  const [timeToggle, setTimeToggle] = useState(true)
+  const changeTime = (time = undefined) => {
+    if (time === undefined) {
+      setTimeToggle(!timeToggle)
+    } else {
+      setTimeToggle(time)
+    }
+  }
   return (
     <div className="App">
       <h2>Toggle</h2>
-      <div className="emoji">{timeToggle ? <span>🌌</span> : <span>💡</span>}</div>
+      <div className="emoji">{timeToggle ? '💡' : '🌌'}</div>
       <div className="button-group">
-        <button onClick={() => setTimeToggle(false)}>Day</button>
-        <button onClick={() => setTimeToggle(true)}>Night</button>
-        <button onClick={() => setTimeToggle(!timeToggle)}>Toggle</button>
+        <button onClick={() => changeTime(true)}>Day</button>
+        <button onClick={() => changeTime(false)}>Night</button>
+        <button onClick={() => changeTime()}>Toggle</button>
       </div>
     </div>
   )
