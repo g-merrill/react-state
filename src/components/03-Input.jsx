b9ca4d266a7fd4c9cc509858b32dc7d9870f3bd9
Greg Merrill <merrillgreg@meta.com>
2025-06-10 10:36:08 -0700

    input

--- src/components/03-Input.jsx
+++ src/components/03-Input.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react"
+
 export default function Input() {
+  const [userInput, setUserInput] = useState('')
+
+  const handleChange = (e) => {
+    setUserInput(e.target.value)
+  }
+
   return (
     <div className='container'>
       <h2>Input</h2>
-      <input type="text" />
+      <input type="text" value={userInput} onChange={handleChange} />
       <h3>The value inside the input is...</h3>
+      <h3>{userInput}</h3>
     </div>
   )
 }
