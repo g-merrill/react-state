7cbe513eb5c407f3784191ac41538b3f66dbe441
Greg Merrill <merrillgreg@meta.com>
2025-06-10 10:14:40 -0700

    update counter with usestate

--- src/components/01-Counter.jsx
+++ src/components/01-Counter.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react"
+
 export default function Counter() {
+  const [count, setCount] = useState(0)
   return (
     <div>
       <h2>Counter</h2>
-      <h3>7</h3>
+      <h3>{count}</h3>
       <div className="button-group">
-        <button>Increase</button>
-        <button>Decrease</button>
+        <button onClick={() => setCount(count + 1)}>Increase</button>
+        <button onClick={() => setCount(count - 1)}>Decrease</button>
       </div>
     </div>
   )
